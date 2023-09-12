306461d236d065b689332c862662e6b2310adf10
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-09-12 15:19:44 +0100

    Add split_consecutive function

--- psyacc/split.py
+++ psyacc/split.py
@@ -9,3 +9,23 @@ def follows(node1, node2):
         and node2 in node1.following()
         and node1.position + 1 == node2.position
     )
+
+
+def split_consecutive(block):
+    """
+    Given a block of nodes at some depth, separate out those which follow one another.
+    """
+    blocks = []
+    current = []
+    depth = block[0].depth
+    for node in block:
+        if node.depth != depth:
+            raise ValueError("Block contains nodes with different depths.")
+        if len(current) == 0 or follows(current[-1], node):
+            current.append(node)
+        else:
+            blocks.append(current)
+            current = [node]
+    if len(current) > 0:
+        blocks.append(current)
+    return blocks
