c51a53fbf09269d1aa1c3f861a693bca96c23f95
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-09-12 15:46:14 +0100

    Add test for split_consecutive error

--- test/test_split.py
+++ test/test_split.py
@@ -2,6 +2,7 @@ from fparser.common.readfortran import FortranStringReader
 from psyclone.psyGen import PSyFactory
 from psyclone.psyir import nodes
 from psyacc.split import follows, split_consecutive
+import pytest
 
 API = "nemo"
 
@@ -15,6 +16,20 @@ _loop_with_3_assignments = """
         a(i) = 0.0
         b(i) = a(i)
         a(i) = 1.0
+      END DO
+    END PROGRAM test
+    """
+
+_double_loop_with_1_assignment = """
+    PROGRAM test
+      REAL :: a(10,10)
+      INTEGER :: i
+      INTEGER :: j
+
+      DO j = 1, 10
+        DO i = 1, 10
+          a(i,j) = 0.0
+        END DO
       END DO
     END PROGRAM test
     """
@@ -44,3 +59,17 @@ def test_split_consecutive(parser):
     assignments = schedule.walk(nodes.Assignment)
     assert len(split_consecutive(assignments)) == 1
     assert len(split_consecutive(assignments[::2])) == 2
+
+
+def test_split_consecutive_valuerror(parser):
+    """
+    Test that a :class:`ValueError` is raised when :func:`split_consecutive` is
+    called with a block of nodes from different depths.
+    """
+    code = parser(FortranStringReader(_double_loop_with_1_assignment))
+    psy = PSyFactory(API, distributed_memory=False).create(code)
+    schedule = psy.invokes.invoke_list[0].schedule
+    loops = schedule.walk(nodes.Loop)
+    expected = "Block contains nodes with different depths."
+    with pytest.raises(ValueError, match=expected):
+        split_consecutive(loops)
