82b806ae956769f53a7128da1033afc6281800e9
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-09-12 16:05:32 +0100

    Add has_kernels_directive function

--- psyacc/acc_kernels.py
+++ psyacc/acc_kernels.py
@@ -1,4 +1,5 @@
 from psyclone.psyir import nodes
+from psyclone.transformations import ACCKernelsDirective
 
 
 def is_outer_loop(loop):
@@ -8,3 +9,11 @@ def is_outer_loop(loop):
     if not isinstance(loop, nodes.Loop):
         raise TypeError(f"Expected a Loop, not '{type(loop)}'.")
     return loop.ancestor(nodes.Loop) is None
+
+
+def has_kernels_directive(node):
+    """
+    Determine whether a node is inside an OpenACC kernels directive.
+    """
+    assert isinstance(node, nodes.Node)
+    return node.ancestor(ACCKernelsDirective)
