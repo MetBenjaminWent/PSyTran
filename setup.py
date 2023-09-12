c8e8b004a48634c37b9964c2ce6df7ee17217c41
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-09-12 14:25:20 +0100

    Add simple setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="psyacc",
+    version="0.1",
+    description="PSy-ACC: tools for automating OpenACC GPU porting efforts using PSyclone",
+    author="Joe Wallwork",
+    author_email="joseph.wallwork@metoffice.gov.uk",
+    packages=["psyacc"],
+)
