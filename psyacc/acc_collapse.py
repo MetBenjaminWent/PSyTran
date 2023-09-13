2fcb6fdbb1b9386b97786d445e77a7cc957a905d
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-09-13 07:58:51 +0100

    Add is_collapsed function

--- psyacc/acc_collapse.py
+++ psyacc/acc_collapse.py
@@ -1,4 +1,6 @@
 from psyclone.psyir import nodes
+from psyacc.acc_kernels import has_kernels_directive
+from psyacc.acc_loop import has_loop_directive
 
 
 def get_ancestors(loop):
@@ -13,3 +15,25 @@ def get_ancestors(loop):
         current = current.ancestor(nodes.Loop)
         ancestors.append(current)
     return ancestors
+
+
+def is_collapsed(loop):
+    """
+    Determine whether a loop lies within a collapsed loop nest.
+
+    :arg loop: the :class:`Loop` node.
+    """
+    if not isinstance(loop, nodes.Loop):
+        raise TypeError(f"Expected a Loop, not '{type(loop)}'.")
+    if not has_kernels_directive(loop):
+        return False
+    ancestors = get_ancestors(loop, inclusive=True)
+    for i, current in enumerate(ancestors):
+        if has_loop_directive(current):
+            loop_dir = current.parent.parent
+            collapse = loop_dir.collapse
+            if collapse is None:
+                continue
+            else:
+                return collapse > i
+    return False
