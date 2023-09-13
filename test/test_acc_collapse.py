33a0e25abda1cf4d2d735221943364954f2a0f2e
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-09-13 08:06:37 +0100

    Test is_collapsed without collapse clause

--- test/test_acc_collapse.py
+++ test/test_acc_collapse.py
@@ -1,6 +1,8 @@
 from fparser.common.readfortran import FortranStringReader
 from psyclone.psyGen import PSyFactory
 from psyclone.psyir import nodes
+from psyacc.acc_kernels import apply_kernels_directive
+from psyacc.acc_loop import apply_loop_directive
 from psyacc.acc_collapse import get_ancestors, is_collapsed
 import code_snippets as cs
 import pytest
@@ -59,5 +61,32 @@ def test_is_collapsed_no_kernels(parser):
     """
     code = parser(FortranStringReader(cs.loop_with_1_assignment))
     psy = PSyFactory("nemo", distributed_memory=False).create(code)
+    loops = psy.invokes.invoke_list[0].schedule.walk(nodes.Loop)
+    assert not is_collapsed(loops[0])
+
+
+def test_is_collapsed_kernels_no_loop(parser):
+    """
+    Test that :func:`is_collapsed` returns ``False`` for a loop with a kernels
+    directive but no loop directives.
+    """
+    code = parser(FortranStringReader(cs.loop_with_1_assignment))
+    psy = PSyFactory("nemo", distributed_memory=False).create(code)
+    loops = psy.invokes.invoke_list[0].schedule.walk(nodes.Loop)
+    apply_kernels_directive(loops[0])
+    loops = psy.invokes.invoke_list[0].schedule.walk(nodes.Loop)
+    assert not is_collapsed(loops[0])
+
+
+def test_is_collapsed_loop_no_collapse(parser):
+    """
+    Test that :func:`is_collapsed` returns ``False`` for a loop with a loop
+    directive but no collapse clause.
+    """
+    code = parser(FortranStringReader(cs.loop_with_1_assignment))
+    psy = PSyFactory("nemo", distributed_memory=False).create(code)
+    loops = psy.invokes.invoke_list[0].schedule.walk(nodes.Loop)
+    apply_kernels_directive(loops[0])
+    apply_loop_directive(loops[0])
     loops = psy.invokes.invoke_list[0].schedule.walk(nodes.Loop)
     assert not is_collapsed(loops[0])
