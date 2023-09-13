a7cc2ce20dad65a4c59e6f98d903d77f5644a7d3
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-09-13 08:34:06 +0100

    Add apply_loop_collapse function

--- psyacc/acc_collapse.py
+++ psyacc/acc_collapse.py
@@ -1,6 +1,6 @@
 from psyclone.psyir import nodes
 from psyacc.acc_kernels import has_kernels_directive
-from psyacc.acc_loop import has_loop_directive
+from psyacc.acc_loop import has_loop_directive, apply_loop_directive
 
 
 def get_ancestors(loop):
@@ -15,6 +15,35 @@ def get_ancestors(loop):
         current = current.ancestor(nodes.Loop)
         ancestors.append(current)
     return ancestors
+
+
+def apply_loop_collapse(loop, collapse):
+    """
+    Apply a collapse clause to a loop.
+
+    :arg loop: the :class:`Loop` node.
+    :arg collapse: the number of loops to collapse
+    """
+    if not isinstance(loop, nodes.Loop):
+        raise TypeError(f"Expected a Loop, not '{type(loop)}'.")
+    if not isinstance(collapse, int):
+        raise TypeError(f"Expected an integer, not '{type(collapse)}'.")
+    if collapse <= 1:
+        raise ValueError(
+            f"Expected an integer greater than one, not {collapse}."
+        )
+    if not has_kernels_directive(loop):
+        raise ValueError(
+            "Cannot apply loop collapse without a kernels directive."
+        )
+    if len(loop.walk(nodes.Loop)) < collapse:
+        raise ValueError(
+            f"Cannot apply collapse to {collapse} loops in a sub-nest of"
+            f" {len(loop.walk(nodes.Loop))}."
+        )
+    if not has_loop_directive(loop):
+        apply_loop_directive(loop)
+    loop.parent.parent._collapse = collapse
 
 
 def is_collapsed(loop):
