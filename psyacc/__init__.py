04de327d52e2a4e65d385e99872e2441e0548f33
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-11-15 14:15:08 +0000

    split_consecutive now in PSyclone as Node.get_sibling_lists

--- psyacc/__init__.py
+++ psyacc/__init__.py
@@ -8,4 +8,3 @@ from psyacc.convert import *  # noqa
 from psyacc.directives import *  # noqa
 from psyacc.family import *  # noqa
 from psyacc.loop import *  # noqa
-from psyacc.split import *  # noqa
