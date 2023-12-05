ec64115adcb2b22fd8421f6e81b563ec1990682b
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-12-05 08:35:16 +0000

    Add assignment to star imports

--- psyacc/__init__.py
+++ psyacc/__init__.py
@@ -3,6 +3,7 @@
 # This file is part of PSyACC and is released under the BSD 3-Clause license.
 # See LICENSE in the root of the repository for full licensing details.
 
+from psyacc.assignment import *  # noqa
 from psyacc.clauses import *  # noqa
 from psyacc.convert import *  # noqa
 from psyacc.directives import *  # noqa
