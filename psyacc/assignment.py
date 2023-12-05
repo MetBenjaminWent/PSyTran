ed42d84779a2fd39380136f24e348cd0562b7090
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2023-12-05 08:34:06 +0000

    Add is_literal_assignment and test

---
+++ psyacc/assignment.py
@@ -0,0 +1,20 @@
+# (C) Crown Copyright, Met Office. All rights reserved.
+#
+# This file is part of PSyACC and is released under the BSD 3-Clause license.
+# See LICENSE in the root of the repository for full licensing details.
+
+from psyclone.psyir import nodes
+
+__all__ = ["is_literal_assignment"]
+
+
+def is_literal_assignment(node):
+    """
+    Determine whether a Node corresponds to an assignment of a literal value.
+
+    :arg node: the :class:`Node` in question
+    """
+    return (
+        isinstance(node, nodes.Assignment)
+        and isinstance(node.rhs, nodes.Literal)
+    )
---
+++ test/test_assignment.py
@@ -0,0 +1,28 @@
+# (C) Crown Copyright, Met Office. All rights reserved.
+#
+# This file is part of PSyACC and is released under the BSD 3-Clause license.
+# See LICENSE in the root of the repository for full licensing details.
+
+from psyclone.psyir import nodes
+from psyacc.assignment import is_literal_assignment
+from utils import *
+import pytest
+
+
+def test_is_literal_assignment(parser):
+    """
+    Test that a :func:`is_literal_assignment` correctly determines a node
+    corresponding to the assignment of a literal value.
+    """
+    schedule = get_schedule(parser, cs.loop_with_1_assignment)
+    assert not is_literal_assignment(schedule.walk(nodes.Loop)[0])
+    assert is_literal_assignment(schedule.walk(nodes.Assignment)[0])
+
+
+def test_is_not_literal_assignment(parser):
+    """
+    Test that a :func:`is_literal_assignment` correctly determines a node
+    not corresponding to the assignment of a literal value.
+    """
+    schedule = get_schedule(parser, cs.loop_with_1_assignment_and_intrinsic_call)
+    assert not is_literal_assignment(schedule.walk(nodes.Assignment)[0])
