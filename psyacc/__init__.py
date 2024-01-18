863e558364ce6a15721a84497b640345ebcefafc
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2024-01-18 17:10:28 +0000

    #13: Testing by adding some incorrect formatting

--- psyacc/__init__.py
+++ psyacc/__init__.py
@@ -1,7 +1,7 @@
 # (C) Crown Copyright, Met Office. All rights reserved.
 #
 # This file is part of PSyACC and is released under the BSD 3-Clause license.
-# See LICENSE in the root of the repository for full licensing details.
+# See LICENSE in the root of the repository for full licensing details. 
 
 from psyacc.assignment import *  # noqa
 from psyacc.clauses import *  # noqa
