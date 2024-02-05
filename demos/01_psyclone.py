5e7287c695d8709a3a3f48b1f6d52122eba6ef8f
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2024-02-05 16:02:21 +0000

    #18: Add first demo

---
+++ demos/01_psyclone.py
@@ -0,0 +1,95 @@
+# Demo 1: PSyclone transformations from the command line
+# ======================================================
+#
+# Here we demonstrate how to run PSyclone in code transformation mode from the command
+# line. Despite being in 'transformation mode', we won't actually apply any code
+# transformations in this demo. We'll simply read some Fortran source code and then
+# write it out to a different file.
+#
+# .. warning::
+#    The first, important thing to note is that these demos are not intended to be run
+#    directly.
+#
+# Instead of running the demos as Python scripts, they should be passed as
+# transformation scripts for the `psyclone` command. This command will only be available
+# if you have activated the virtual environment you used to install PSyclone (and
+# PSyACC).
+#
+# The recommended command for this demo is as follows.
+#
+# .. code-block::
+#    psyclone -api nemo --config ../.psyclone/psyclone.cfg \
+#        fortran/empty.F90 --script ./01_psyclone.py -opsy outputs/01_psyclone.F90
+#
+# There is a lot to unpack here! Let's work through the arguments one by one.
+#
+# API
+# ---
+#
+# PSyclone requires an API to be chosen, i.e., the argument following `-api`. Since
+# PSyACC is designed to use the NEMO API, we should always choose this.
+#
+# Configuration
+# -------------
+#
+# PSyclone also requires a configuration file, i.e., the argument following `--config`.
+# For simplicity, we provide the path to a copy of the default one used by PSyclone.
+# However, feel free to change this or modify it, as desired. Editing the PSyclone
+# configuration is considered advanced, so don't worry about this if you are only just
+# getting started.
+#
+# Source code
+# -----------
+#
+# Perhaps the most important argument is the source code to be transformed. We provide
+# a subdirectory of Fortran source to be used for this purpose. In this demo, we parse
+# the `empty.F90` file, which contains a 'Hello, World!' Fortran program:
+#
+# .. code-block:: fortran
+#    PROGRAM intro
+#      IMPLICIT NONE
+#      PRINT *, "Hello, Fortran World!"
+#    END PROGRAM intro
+#
+# Transformation script
+# ---------------------
+#
+# The `--script` option is used to provide a transformation script to PSyclone. In this
+# case, we pass this file. Given that almost all of it is comprised of comments, the
+# only Python syntax that will be picked up is the following. ::
+
+
+def trans(psy):
+    print("Hello, PSyACC World!")
+    return psy
+
+
+# The transformation function *must* have the name `trans`, *must* have a single
+# argument (the :class:`~.PSy` instance), and *must* return the same :class:`~.PSy`
+# instance, usually modified in some way. In this demo, we don't actually make any
+# modifications and instead just say hello to the user.
+#
+# Output file
+# -----------
+#
+# Finally, we need to tell PSyclone where to write the transformed code to. If this
+# argument is dropped then the transformed code will be printed to screen. We pass the
+# location `outputs/01_psyclone-empty.F90` as a combination of the names of the source
+# file and transformation script. The `output` subdirectory should have been created
+# when you installed PSyACC.
+#
+# Inspecting the output file, it should look something like.
+#
+# .. code-block:: fortran
+#    program intro
+#
+#      PRINT *, "Hello, Fortran World!"
+#
+#    end program intro
+#
+# It doesn't take a Fortran expert to realise that running the two versions of the
+# Fortran code will give the same result. However, the files themselves are slightly
+# different, even though the transformation function that we used was trivial. The
+# reason for this is that PSyclone always reformats code according to its in-built
+# preferences. In particular, it will use lower case (for the most part) and will
+# put a single blank line at the start and end of any program.
