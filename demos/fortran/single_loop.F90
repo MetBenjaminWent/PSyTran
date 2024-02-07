5ce53c4631c1950e8234a62e05e91e12941230ff
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2024-02-07 13:31:58 +0000

    #29: Reformat Fortran code

--- demos/fortran/single_loop.F90
+++ demos/fortran/single_loop.F90
@@ -1,10 +1,10 @@
 PROGRAM single_loop
-   IMPLICIT NONE
-   INTEGER, PARAMETER :: n = 10
-   INTEGER :: i
-   REAL :: arr(n)
+  IMPLICIT NONE
+  INTEGER, PARAMETER :: n = 10
+  INTEGER :: i
+  REAL :: arr(n)
 
-   DO i = 1, n
-     arr(i) = 0.0
-   END DO
+  DO i = 1, n
+    arr(i) = 0.0
+  END DO
 END PROGRAM single_loop
