71ab05d155628b6146436ad6d13d2937a6fd96ab
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2024-02-08 14:24:25 +0000

    #36: Inline _is_perfect_nest_iter

--- psyacc/loop.py
+++ psyacc/loop.py
@@ -34,15 +34,6 @@ def is_outer_loop(loop):
     return loop.ancestor(nodes.Loop) is None
 
 
-def _perfect_nest_iter(loops, non_loops):
-    """
-    Determine whether a nest iteration is perfect.
-    """
-    return (len(loops) == 1 and not non_loops) or (
-        not loops and not (any([node.walk(nodes.Loop) for node in non_loops]))
-    )
-
-
 def is_perfectly_nested(loop):
     """
     Determine whether a loop nest is perfectly nested, i.e., each level except
@@ -63,7 +54,10 @@ def is_perfectly_nested(loop):
     while len(loops) > 0:
         non_loops = get_children(loops[0], exclude=exclude)
         loops = get_children(loops[0], node_type=nodes.Loop)
-        if not _perfect_nest_iter(loops, non_loops):
+        if not (
+            (len(loops) == 1 and not non_loops)
+            or (not loops and not (any([node.walk(nodes.Loop) for node in non_loops])))
+        ):
             return False
     else:
         return True
