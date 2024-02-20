3ff12a3bcf162d2d2112b476a5c5ee5965a13ca0
Joe Wallwork <joseph.wallwork@metoffice.gov.uk>
2024-02-20 09:41:19 +0000

    #17: Use mkdir -p

--- set_default_config.sh
+++ set_default_config.sh
@@ -6,5 +6,5 @@
 # See LICENSE in the root of the repository for full licensing details.
 
 PSYCLONE_DIR=$(python3 -c "import psyclone; print(psyclone.__path__[0])")/../..
-mkdir ${VIRTUAL_ENV}/share/psyclone
+mkdir -p ${VIRTUAL_ENV}/share/psyclone
 cp ${PSYCLONE_DIR}/config/psyclone.cfg ${VIRTUAL_ENV}/share/psyclone
