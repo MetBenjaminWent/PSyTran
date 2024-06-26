13a03eb27f0347dd174abc6d7b67e5b0e54ef603
Joe Wallwork <jw2423@cam.ac.uk>
2024-06-26 18:18:39 +0100

    #60: Update and lint for test_convert

--- test/test_convert.py
+++ test/test_convert.py
@@ -3,13 +3,20 @@
 # This file is part of PSyACC and is released under the BSD 3-Clause license.
 # See LICENSE in the root of the repository for full licensing details.
 
-from psyclone.psyir import nodes
-from utils import *
+"""
+Unit tests for PSyACC's `convert` module.
+"""
+
 import pytest
+import code_snippets as cs
+from utils import get_schedule
+from psyclone.psyir import nodes
+from psyacc.convert import convert_array_notation, convert_range_loops
 
 
-@pytest.fixture(params=[1, 2, 3])
-def dim(request):
+@pytest.fixture(name="dim", params=[1, 2, 3])
+def fixture_dim(request):
+    """Pytest fixture for spatial dimension."""
     return request.param
 
 
@@ -26,26 +33,28 @@ implied_array_assignment = {
 }
 
 
-def test_convert_array_notation(parser, dim):
+def test_convert_array_notation(fortran_reader, dim):
     """
     Test that :func:`convert_array_notation` successfully converts an implied
     array range assignment into an explicit one.
     """
-    schedule = get_schedule(parser, implied_array_assignment[dim])
+    schedule = get_schedule(fortran_reader, implied_array_assignment[dim])
     assert len(schedule.walk(nodes.Assignment)) == 1
     assert len(schedule.walk(nodes.Range)) == 0
     convert_array_notation(schedule)
     assert len(schedule.walk(nodes.Assignment)) == 1
     assert len(schedule.walk(nodes.Range)) == dim
-    assert str(schedule) == str(get_schedule(parser, array_assignment[dim]))
+    assert str(schedule) == str(
+        get_schedule(fortran_reader, array_assignment[dim])
+    )
 
 
-def test_avoid_array_notation_subroutine(parser):
+def test_avoid_array_notation_subroutine(fortran_reader):
     """
     Test that :func:`convert_array_notation` does not use array notation in
     subroutine calls.
     """
-    schedule = get_schedule(parser, cs.subroutine_call)
+    schedule = get_schedule(fortran_reader, cs.subroutine_call)
     assert len(schedule.walk(nodes.Call)) == 1
     assert len(schedule.walk(nodes.Range)) == 0
     convert_array_notation(schedule)
@@ -53,13 +62,13 @@ def test_avoid_array_notation_subroutine(parser):
     assert len(schedule.walk(nodes.Range)) == 0
 
 
-def test_convert_range_loops(parser, dim):
+def test_convert_range_loops(fortran_reader, dim):
     """
     Test that :func:`convert_range_loops` successfully converts an array range
     assignment into a loop. If dim > 1 then the loop should itself contain an
     array range assignment.
     """
-    schedule = get_schedule(parser, array_assignment[dim])
+    schedule = get_schedule(fortran_reader, array_assignment[dim])
     assert len(schedule.walk(nodes.Assignment)) == 1
     assert len(schedule.walk(nodes.Loop)) == 0
     convert_range_loops(schedule)
