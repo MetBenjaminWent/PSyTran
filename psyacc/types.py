e8bfc5280146751dd3d00b5c23b754c97e92bf2e
Joe Wallwork <jw2423@cam.ac.uk>
2024-11-13 14:36:53 +0000

    Use is_character and extend tests

--- psyacc/types.py
+++ psyacc/types.py
@@ -14,29 +14,40 @@ from psyacc.family import get_descendents
 __all__ = ["is_character", "refers_to_character"]
 
 
-def is_character(ref):
+def is_character(ref, unknown_as=None):
     """
     Determine whether a Reference Node is of type ``CHARACTER``.
 
     :arg ref: the Node to check.
     :type ref: :py:class:`Reference`
+    :kwarg unknown_as: Determines behaviour in the case where it cannot be
+        determined whether the DataNode is a character. Defaults to None,
+        in which case an exception is raised.
+    :type unknown_as: Optional[bool]
 
     :returns: ``True`` if the Reference is to a ``CHARACTER``, else ``False``.
     :rtype: :py:class:`bool`
     """
     assert isinstance(ref, Reference)
-    return "CHARACTER" in ref.datatype.type_text
+    return ref.is_character(unknown_as=unknown_as)
 
 
-def refers_to_character(node):
+def refers_to_character(node, unknown_as=None):
     r"""
     Determine whether a Node contains References to ``CHARACTER``\s.
 
     :arg node: the Node to check.
     :type node: :py:class:`Node`
+    :kwarg unknown_as: Determines behaviour in the case where it cannot be
+        determined whether the DataNode is a character. Defaults to None,
+        in which case an exception is raised.
+    :type unknown_as: Optional[bool]
 
     :returns: ``True`` if there are References to ``CHARACTER``\s, else
         ``False``.
     :rtype: :py:class:`bool`
     """
-    return any(is_character(ref) for ref in get_descendents(node, Reference))
+    return any(
+        is_character(ref, unknown_as=unknown_as)
+        for ref in get_descendents(node, Reference)
+    )
--- test/test_types.py
+++ test/test_types.py
@@ -7,6 +7,8 @@
 Unit tests for members of PSyACC's `types` module.
 """
 
+import pytest
+
 from psyclone.psyir import nodes
 from utils import get_schedule
 
@@ -14,11 +16,28 @@ import code_snippets as cs
 from psyacc.types import is_character, refers_to_character
 
 
-def test_is_character(fortran_reader):
+def test_is_character_error_string(fortran_reader):
     """
-    Test that :func:`is_character` correctly determines variables of intrinsic
-    type `CHARACTER`.
+    Test that :func:`is_character` and :func:`refers_to_character` raise
+    exceptions as expected when applied to a *string* assignment with the
+    `unknown_as` parameter unset.
     """
     schedule = get_schedule(fortran_reader, cs.string_assignment)
+    expected = (
+        "is_character could not resolve whether the expression 'c' operates on"
+        " characters."
+    )
+    with pytest.raises(ValueError, match=expected):
+        is_character(schedule.walk(nodes.Reference)[0])
+    with pytest.raises(ValueError, match=expected):
+        refers_to_character(schedule)
+
+
+def test_is_character_char(fortran_reader):
+    """
+    Test that :func:`is_character` and :func:`refers_to_character` correctly
+    determine variables of intrinsic type `CHARACTER`.
+    """
+    schedule = get_schedule(fortran_reader, cs.char_assignment)
     assert is_character(schedule.walk(nodes.Reference)[0])
     assert refers_to_character(schedule)
