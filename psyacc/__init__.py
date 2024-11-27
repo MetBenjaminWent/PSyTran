9ef4371c1ee35cb9dee41a97a2282c5ca0ea7b4b
Joe Wallwork <jw2423@cam.ac.uk>
2024-11-27 16:20:51 +0000

    Drop types module (unused)

--- psyacc/__init__.py
+++ psyacc/__init__.py
@@ -12,4 +12,3 @@ from psyacc.convert import *  # noqa
 from psyacc.directives import *  # noqa
 from psyacc.family import *  # noqa
 from psyacc.loop import *  # noqa
-from psyacc.types import *  # noqa
